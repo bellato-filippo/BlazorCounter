2bae56b0ca4925bb7539d83353e5030b9a594ed6
Stage01 <stage.etics@hotmail.com>
2021-09-07 12:58:00 +0200

    aggiunto modello client

---
+++ BlazorCounter/Client/Models/ProductCli.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCounter.Client.Models;
+public class ProductCli
+{
+    [Key]
+    public int Id {  get; set; }
+    public string? Name {  get; set; }
+    public int Price {  get; set; }
+}
