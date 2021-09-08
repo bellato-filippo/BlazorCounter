784edc9f1a58d17e7e5d2a0607f857811e703d50
Stage01 <stage.etics@hotmail.com>
2021-09-08 10:05:59 +0200

    rinunciato al login

--- BlazorCounter/Client/Pages/Index.razor.cs
+++ BlazorCounter/Client/Pages/Index.razor.cs
@@ -1,31 +1,5 @@
-﻿using BlazorCounter.Client.Models;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http.Json;
-
-namespace BlazorCounter.Client.Pages;
+﻿namespace BlazorCounter.Client.Pages;
 
 public partial class Index
 {
-    [Inject]
-    public HttpClient Http {  get; set; }
-    [Inject]
-    public NavigationManager NavigationManager {  get; set; }
-
-    private string apiUrl = @"http://localhost:5002/api/authser";
-    public void Redirect()
-    {
-        NavigationManager.NavigateTo("login");
-    }
-    public AuthCli Temp;
-    protected override Task OnInitializedAsync()
-    {
-        return CaricaEventi();
-    }
-
-    private async Task CaricaEventi()
-    {
-        Temp = await Http.GetFromJsonAsync<AuthCli>($"{apiUrl}/{1}");
-    }
 }
