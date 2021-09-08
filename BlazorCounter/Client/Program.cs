48c0d8ec937513d586af5433b3adecddeb251e99
Stage01 <stage.etics@hotmail.com>
2021-09-08 16:26:03 +0200

    aggiunti alcuni sfbutton

--- BlazorCounter/Client/Program.cs
+++ BlazorCounter/Client/Program.cs
@@ -1,9 +1,12 @@
 using BlazorCounter.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
